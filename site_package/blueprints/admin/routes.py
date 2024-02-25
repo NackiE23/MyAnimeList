ac7aba479ccfce9e12e0b6b8edf723b5eaea419c
NackiE23 <dimavarchuc@gmail.com>
2024-02-25 11:06:59 +0200

    Make admin with classes

--- site_package/blueprints/admin/routes.py
+++ site_package/blueprints/admin/routes.py
@@ -3,23 +3,24 @@ from flask import Blueprint, render_template, abort
 from site_package.models.media import Media, MediaType, MediaCategory, RelatedMedia, RelationCategory
 from site_package.models.user import User
 
-admin_bp = Blueprint('admin_bp', __name__)
+from .control import Admin, AdminModel
 
-models = [
-    # (Model, *order_by field*)
-    (User, 'name'),
-    (Media, 'name'),
-    (MediaType, 'name'),
-    (MediaCategory, 'name'),
-    (RelatedMedia, 'id'),
-    (RelationCategory, 'name')
-]
+admin_bp = Blueprint('admin_bp', __name__)
+admin = Admin()
+admin.add_models([
+    AdminModel(User, 'name'),
+    AdminModel(Media, 'name'),
+    AdminModel(MediaType, 'name'),
+    AdminModel(MediaCategory, 'name'),
+    AdminModel(RelatedMedia, 'id'),
+    AdminModel(RelationCategory, 'name'),
+])
 
 
 @admin_bp.route("/", methods=["GET"])
 def main():
     context = {
-        "models": models,
+        "admin": admin,
     }
 
     return render_template("admin/base.html", **context)
@@ -27,15 +28,12 @@ def main():
 
 @admin_bp.route("/<model_name>", methods=["GET"])
 def model_info(model_name):
-    for model, order_by in models:
-        if model_name.lower() == model.__name__.lower():
-            current_model = (model, order_by)
-            break
-    else:
+    current_model = admin.get_model_by_name(model_name)
+    if not current_model:
         abort(404)
 
     context = {
-        "models": models,
+        "admin": admin,
         "current_model": current_model,
     }
 
